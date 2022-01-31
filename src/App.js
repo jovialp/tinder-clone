9ab1bf32eb7eb819272e96bd65f6af028da63d09
jovialp <jvlthms@gmail.com>
2022-01-31 23:57:03 +0530

    App created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+// Components
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      App
+      {/* Header */}
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+
+const Header = () => {
+  return (
+    <div>
+      {' '}
+      header
+      <PersonIcon />
+    </div>
+  );
+};
+
+export default Header;
