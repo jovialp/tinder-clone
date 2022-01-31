d169b0f21fe10d0fa94908d048a8fbe6c706643a
jovialp <jvlthms@gmail.com>
2022-02-01 00:22:38 +0530

    Header Started

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from './components/Header';
 export default function App() {
   return (
     <div>
-      App
       {/* Header */}
       <Header />
     </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import { Forum as ForumIcon, Person as PersonIcon } from '@mui/icons-material';
+
+import './header.css';
 
 const Header = () => {
   return (
-    <div>
-      {' '}
-      header
-      <PersonIcon />
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+        alt="logo"
+      />
+
+      <ForumIcon fontSize="large" className="header__icon" />
     </div>
   );
 };
